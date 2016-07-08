0d81e17ae85d87b9f8cab0338fdf533dd52c2642
rucode <ronaldu@gmail.com>
2016-07-08 02:12:40 -0300

    nueva version

---
+++ Ejercidio_integrando_material_sesign/app/src/main/java/com/rubach/ejercidio_integrando_material_sesign/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rubach.ejercidio_integrando_material_sesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Ejercidio_integrando_material_sesign/app/src/test/java/com/rubach/ejercidio_integrando_material_sesign/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.rubach.ejercidio_integrando_material_sesign;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
